c909b2c796fd1ad52b0a38ff8b362c46b7cb253e
CeydaOzturkk <ceydaozturk55@gmail.com>
2021-06-27 19:53:04 +0300

    Giris yap ve kayit ol kisimlari eklendi.

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+Create Database Sign
+
+Create Table information(
+id int primary key identity (1,1),
+tc nvarchar (11),
+cinsiyet nvarchar(2),
+ad nvarchar(10),
+soyad nvarchar(10),
+telefon nvarchar (10),
+eposta nvarchar(50),
+sifre nvarchar(15),
+sifre_tekrar nvarchar(15))
+
